f3605cb9428eaca0841298dfaf78e4a55080a5a2
C0deNe0 <access.naveenhiremath@gmail.com>
2025-04-06 16:19:06 +0530

    completed with the linkedlist topic

--- StackQueue/LinkedListImplementation.java
+++ StackQueue/LinkedListImplementation.java
@@ -25,7 +25,7 @@ class LinkedList{
         return;
     }
 
-
+    
 
 
     void disp(){
@@ -38,7 +38,71 @@ class LinkedList{
         }
         System.out.println("the number of nodes are: "+(count+1));
     }
-}
+
+
+    void insertAtFirst(int i,int data){
+        Node x=root;
+        Node n=new Node(data);
+        if(i==0){
+            n.add=x;
+            root=n;
+            count++;
+            return;
+        }
+    } 
+    void insertInBetween(int i,int data){
+        Node x=root;
+        Node n= new Node(data);
+        if(i==0){
+            n.add =x;
+            root =n;
+            return;
+        }
+        int p=0;
+        while(p < i){
+            x=x.add;
+           p++ ;
+        }
+        n.add = x.add;
+        x.add=n;  
+        count++;                                   
+        return;
+    }
+
+    void sort(){
+
+        for(Node i=root;i.add !=null;i=i.add){
+            for(Node j=i.add;j !=null;j=j.add){
+                    if(i.data> j.data){
+                        int t= i.data;
+                        i.data =j.data;
+                        j.data =t;
+                    }
+                }
+            }
+        }
+
+
+    void delete(int i){
+        Node x=root;
+        if(i<0) i=0;
+        if(i==0){
+            root=x.add;
+        }
+
+        while (i!=1) {
+            x=x.add;
+            i--;
+        }
+        Node y=x.add;
+        x.add = y.add;
+        count--;
+        return;
+        
+    }
+            
+    }
+
 
 
 public class LinkedListImplementation {
@@ -50,6 +114,12 @@ public class LinkedListImplementation {
         l.append(40);
         l.append(50);
         l.append(20);
+        l.insertAtFirst(0, 200);
+        l.insertInBetween(4, 69);
+        l.disp();
+        l.delete(2);
+        l.disp();
+        l.sort();
         l.disp();
     }
 }
